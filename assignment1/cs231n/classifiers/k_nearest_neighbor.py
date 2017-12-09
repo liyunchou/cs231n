52c1f78db9c33ee608d2db7935a8a8c887958d89
doge <liyunchou94@163.com>
2017-12-09 13:20:41 +0000

    Assignment1 finished on cloud

--- assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -74,7 +74,7 @@ class KNearestNeighbor(object):
         # not use a loop over dimension.                                    #
         #####################################################################
         #pass
-        dists[i, j] = np.sqrt(np.sums(np.square(X[i] - self.X_train[j])))
+        dists[i, j] = np.sqrt(np.sum(np.square(X[i] - self.X_train[j])))
         #####################################################################
         #                       END OF YOUR CODE                            #
         #####################################################################
@@ -127,8 +127,8 @@ class KNearestNeighbor(object):
     #########################################################################
     #pass
     dists = np.sqrt(-2 * np.dot(X, self.X_train.T) + \
-      np.sums(np.square(self.X_train), axis = 1) + \
-      np.sums(np.square(X), axis = 1)[:,None])
+      np.sum(np.square(self.X_train), axis = 1) + \
+      np.sum(np.square(X), axis = 1)[:,None])
 
     # dists = np.sqrt(-2*np.dot(X, self.X_train.T) + \
     #   np.sum(np.square(self.X_train), axis = 1) + \
@@ -166,7 +166,7 @@ class KNearestNeighbor(object):
       # Hint: Look up the function numpy.argsort.                             #
       #########################################################################
       #pass
-      closest_y = self.y_train[np.argsort(dist[i])[:k]]
+      closest_y = self.y_train[np.argsort(dists[i])[:k]]
       #########################################################################
       # TODO:                                                                 #
       # Now that you have found the labels of the k nearest neighbors, you    #
